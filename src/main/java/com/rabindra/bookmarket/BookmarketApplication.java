a9f627a400ea857e8676369eda02541e0eb5373c
rabindra <rabindra@red27mobile.com>
2022-09-11 00:57:30 +0545

    setting default profile active

--- src/main/java/com/rabindra/bookmarket/BookmarketApplication.java
+++ src/main/java/com/rabindra/bookmarket/BookmarketApplication.java
@@ -2,11 +2,13 @@ package com.rabindra.bookmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 //@EnableSwagger2
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class BookmarketApplication {
 
 	public static void main(String[] args) {
