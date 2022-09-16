978c79df141adf7ee113f61c03104ba00c1d3d26
rabindra <rabindra@red27mobile.com>
2022-09-16 21:19:59 +0545

    adding jsonwebtoken dependency

--- src/main/java/com/rabindra/bookmarket/security/jwt/JwtProvider.java
+++ src/main/java/com/rabindra/bookmarket/security/jwt/JwtProvider.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
